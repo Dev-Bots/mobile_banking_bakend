ebb15a80874ee7e836114dabf96d2ef96e90a009
Tinsae <itistinsae@gmail.com>
2021-09-01 21:38:59 +0300

    converting account type to client model

--- features/loan/app.py
+++ features/loan/app.py
@@ -15,3 +15,22 @@ class LoanSchema(Resource):
 
             if not active_loan:
                 args = loan_args.parse_args()
+                current_user = Client.query.get(current_user.id)
+                if current_user.get_account_type() == 'gold':
+                    days = GOLD_DAYS
+                    amount_limit = GOLD_LOAN_AMOUNT
+                    interest_rate = GOLD_RATE
+                elif current_user.get_account_type() == 'silver':
+                    days = SILVER_DAYS
+                    amount_limit = SILVER_LOAN_AMOUNT
+                    interest_rate = SILVER_RATE
+                else:
+                    days = BRONZE_DAYS
+                    amount_limit = BRONZE_LOAN_AMOUNT
+                    interest_rate = BRONZE_RATE
+
+                print(amount_limit, args['amount'])
+                if args['amount'] <= amount_limit:
+
+                    new_loan = Loan(current_user.account_number, args["amount"], datetime.datetime.utcnow() + datetime.timedelta(days=days), interest_rate)
+                    central_account = Admin.query.filter(Admin.account_number == CENTRAL_ACCOUNT_NUMBER).first()
