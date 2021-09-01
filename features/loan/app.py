19d862a6ccb7ecff06fddcdabcb3af7b83f1638a
Tinsae <itistinsae@gmail.com>
2021-09-01 20:41:07 +0300

    check if the client doesn't already got an unpaid loan

--- features/loan/app.py
+++ features/loan/app.py
@@ -7,3 +7,11 @@ from constants import *
 class LoanSchema(Resource):
 
     method_decorators = [token_required]
+    def post(self, current_user):
+        
+        if current_user.get_role() == 'client':
+            
+            active_loan = Loan.query.filter(Loan.account_number == current_user.account_number, Loan.is_active == True).all()
+
+            if not active_loan:
+                args = loan_args.parse_args()
