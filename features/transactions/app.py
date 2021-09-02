ac0eb7d170f69cba33534cfc8611dbb70735acc6
kevin-shitaye <it.kevin.shitaye@gmail.com>
2021-09-02 22:17:21 +0300

    contact save and remove for clients

--- features/transactions/app.py
+++ features/transactions/app.py
@@ -29,3 +29,38 @@ class AccountSchema(Resource):
             elif current_user != account:
                 return jsonify(account.serialize_general_info()) 
         return make_response({"message": "something wrong"}, 400)
+#save customer(contacts) with get method and remove with delete method
+class SaveAccount(Resource):
+
+    method_decorators = [token_required]
+
+    def put(self, current_user, account_number):
+
+        account = Client.query.filter(Client.account_number == account_number).first()
+        current_user = Client.query.get(current_user.id)
+        if account and account is not current_user:
+            current_user.beneficiaries.append(account)
+            
+            db.session.add(account)
+            db.session.add(current_user)
+            db.session.commit()
+            
+            return make_response({"message": f"Account({account.account_number}) saved."}, 201)
+        return make_response({"message": "Account does not exist."}, 404)
+
+    def delete(self, current_user, account_number):
+
+        account = Client.query.filter(Client.account_number == account_number).first()
+        current_user = Client.query.get(current_user.id)
+
+        if account and account is not current_user:
+  
+            if account in list(current_user.beneficiaries):
+                current_user.beneficiaries.remove(account)
+                
+                db.session.add(current_user)
+                db.session.add(account)
+                db.session.commit()
+                return make_response({"message": f"Account({account.account_number}) removed."}, 202)
+            return make_response({"message": "Account is not in saved list."}, 404)
+        return make_response({"message": "Account does not exist."}, 404)
